722ee4edbcac3b02206b7f503f2097b294b5e8d4
Rifqin-11 <rifqinaufal9009@gmail.com>
2025-07-04 00:48:25 +0700

    feat: add automate delete data

--- models/sharedBill.js
+++ models/sharedBill.js
@@ -5,7 +5,14 @@ const sharedBillSchema = new mongoose.Schema(
     billData: Object,
     people: Array,
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
+);
+
+sharedBillSchema.index(
+  { createdAt: 1 },
+  { expireAfterSeconds: 60 * 60 * 24 * 30 }
 );
 
 export default mongoose.model("SharedBill", sharedBillSchema);
